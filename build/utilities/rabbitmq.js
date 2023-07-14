4a762a5303e6f9e727959ed0af7209659b02799b
Finn <ernzerhofffinn@gmail.com>
2023-07-14 16:35:28 +0200

    Delete

--- build/utilities/rabbitmq.js
+++
@@ -1,6 +0,0 @@
-import { connect } from "amqplib";
-const connection = await connect('amqp://zetax:zetaxiscool@141.144.236.205/');
-export const channel = await connection.createChannel();
-await channel.assertQueue('matchmaker');
-await channel.purgeQueue('matchmaker');
-//# sourceMappingURL=rabbitmq.js.map
